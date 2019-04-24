addb377c6a18e004686841f925c9789d7263294b
Mark Moll <mdmoll@gmail.com>
2019-04-24 08:44:19 -0400

    More Lomboking

--- src/main/java/com/group3/budgetApp/model/Account.java
+++ src/main/java/com/group3/budgetApp/model/Account.java
@@ -1,4 +1,5 @@
 package com.group3.budgetApp.model;
+
 import lombok.Data;
 
 import javax.persistence.*;
@@ -21,19 +22,21 @@ public class Account {
     private Integer accountTypeId;
     @Column(name = "nickname")
     private String nickname;
-
-    public Account(){
+    
+    public Account() {
     }
-    public Account(Integer id){
+    
+    public Account(Integer id) {
         this.id = id;
     }
+    
     public Account(String name, Integer user_id) {
         this.name = name;
         this.userId = user_id;
         this.balance = 0.0;
         this.institutionName = "";
     }
-
+    
     public Account(String name, Double balance, Integer user_id, String institution_name, Integer accountTypeId, String nickname) {
         this.name = name;
         this.balance = balance;
@@ -41,45 +44,5 @@ public class Account {
         this.institutionName = institution_name;
         this.accountTypeId = accountTypeId;
         this.nickname = nickname;
-    }
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public Double getBalance() {
-        return balance;
-    }
-
-    public void setBalance(Double balance) {
-        this.balance = balance;
-    }
-
-    public Integer getUser_id() {
-        return userId;
-    }
-
-    public void setUser_id(Integer user_id) {
-        this.userId = user_id;
-    }
-
-    public String getInstitution_name() {
-        return institutionName;
-    }
-
-    public void setInstitution_name(String institution_name) {
-        this.institutionName = institution_name;
     }
 }
